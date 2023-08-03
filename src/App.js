16382a58060871d2d3028db2505dcb4c4b5fe4f1
xuel <butter202209@163.com>
2023-08-03 12:09:44 +0800

    feat: react key demo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,53 @@
-import "./styles.css";
+import React, { useState } from 'react';
 
-export default function App() {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-  );
-}
+const TodoList = ({ todos }) => {
+    return (
+        <ul>
+            {todos.map((todo) => (
+                <TodoItem
+                    key={todo.id}
+                    todo={todo} />
+            ))}
+        </ul>
+    );
+};
+
+const TodoItem = ({ todo }) => {
+    const [completed, setCompleted] = useState(todo.completed);
+
+    const handleToggleComplete = () => {
+        setCompleted(!completed);
+    };
+
+    return (
+        <li>
+            <input type="checkbox" checked={completed} onChange={handleToggleComplete} />
+            <span style={{ textDecoration: completed ? 'line-through' : 'none' }}>{todo.text}</span>
+        </li>
+    );
+};
+
+const App = () => {
+    const initialTodos = [
+        { id: 1, text: 'Learn React', completed: false },
+        { id: 2, text: 'Build a Todo App', completed: true },
+        { id: 3, text: 'Deploy to production', completed: false },
+    ];
+
+    const [todos, setTodos] = useState(initialTodos);
+
+    const handleReorder = () => {
+        const shuffledTodos = [...todos];
+        shuffledTodos.sort(() => Math.random() - 0.5); // 随机排序
+        setTodos(shuffledTodos);
+    };
+
+    return (
+        <div>
+            <button onClick={handleReorder}>重新排序</button>
+            <TodoList todos={todos} />
+        </div>
+    );
+};
+
+export default App;
